45cb720e3551d7d0192744fa27dea576a7e0f8cb
jgt922 <jgt922@alumnos.unican.es>
2022-02-02 17:57:53 +0100

    Portfolio

--- django_portafolio/settings.py
+++ django_portafolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*x=!l*^6s5=jwojrzbm!o^nldn87&tf(5e_#u-ig-4*pi^q#mp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
